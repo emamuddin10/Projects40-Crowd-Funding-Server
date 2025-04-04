65daa07122c8ebc0a1b513e2c69a05ef6161cd42
Emam Uddin <emamskingdom@gmail.com>
2025-04-05 02:11:13 +0600

    added delete

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const express = require("express")
-const app = express()
-const cors = require('cors')
-const port = process.env.PORT || 5000
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 // crowdFunding
 // F9TfQ7LsfKJ4Iunk
 
-
-const uri = "mongodb+srv://crowdFunding:F9TfQ7LsfKJ4Iunk@cluster0.v28xn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri =
+  "mongodb+srv://crowdFunding:F9TfQ7LsfKJ4Iunk@cluster0.v28xn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -19,43 +19,64 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const fundingDBCollection = client.db('fundingDB').collection('funding')
+    const fundingDBCollection = client.db("fundingDB").collection("funding");
 
+    // get api for my campaign
 
-    // get api for my campaign 
-   
-
-    // get for details 
-    app.get('/details-campaign/:id', async(req,res)=>{
-      const paramId = req.params.id 
-      const query = {_id: new ObjectId(paramId)}
-      const result = await fundingDBCollection.findOne(query)
-      res.send(result)
-    })
+    app.get("/my-campaign/:email", async (req, res) => {
+      const params = req.params.email;
+      const query = { email: params };
+      const result = await fundingDBCollection.find(query).toArray();
+      res.send(result);
+    });
+    // get for details
+    app.get("/details-campaign/:id", async (req, res) => {
+      const paramId = req.params.id;
+      const query = { _id: new ObjectId(paramId) };
+      const result = await fundingDBCollection.findOne(query);
+      res.send(result);
+    });
 
     // get for read that data
-    app.get('/allCampaign',async(req,res)=>{
-      const result =await fundingDBCollection.find().toArray()
-      res.send(result)
-    })
-    // post 
-    app.post('/addCampaign',async(req,res)=>{
-      const data = req.body
-      console.log(data)
-      const result = await fundingDBCollection.insertOne(data)
-      res.send(result)
-    })
+    app.get("/allCampaign", async (req, res) => {
+      const result = await fundingDBCollection.find().toArray();
+      res.send(result);
+    });
+    // post
+    app.post("/addCampaign", async (req, res) => {
+      const data = req.body;
+      console.log(data);
+      const result = await fundingDBCollection.insertOne(data);
+      res.send(result);
+    });
+
+    // update campaign
+    app.patch('/update/:id',async(req,res)=>{
+      const paramsId = req.params.id 
+      const filter = {_id: new ObjectId(paramsId)}
+      const options = { upsert: true };
+      
+    }) 
+
+    //  delete for my campaign
+     app.delete("/delete-campaign/:id", async (req, res) => {
+      const paramsId = req.params.id;
+      const query = { _id: new ObjectId(paramsId) };
+      const result = await fundingDBCollection.deleteOne(query);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -63,12 +84,9 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/',(req,res)=>{
-    res.send('the server is running successfuly browser')
-})
-app.listen(port,()=>{
-    console.log(`server is running on port: ${port}`)
-})
+app.get("/", (req, res) => {
+  res.send("the server is running successfuly browser");
+});
+app.listen(port, () => {
+  console.log(`server is running on port: ${port}`);
+});
